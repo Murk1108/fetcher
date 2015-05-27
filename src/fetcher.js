cc4a14e2820f8024ece32f4aa2b6b5ba4ca2f973
Markus Wagner <murk@ferrum-et-magica.de>
2015-05-27 20:52:52 +0200

    Move logic to seperate file

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,25 +1,223 @@
 var util = require('util')
+  , crypton = require('crypto')
+  , fs = require('fs')
+  , http = require('http')
+  , path = require('path')
+  , spawn = require('child_process').spawn
   , EventEmitter = require('events').EventEmitter;
 
-var Fetcher = function() {
-	EventEmitter.call(this);
+var Fetcher = function(config) {
+  var self = this;
+	EventEmitter.call(self);
+  self.config = config || {};
+
+  self.files = [];
+  self.current_file = 0;
+  self.payload_ready = false;
 };
 
 util.inherits(Fetcher, EventEmitter);
 
+Fetcher.prototype._payload_update_completed = function () {
+  var self = this;
+  self.payload_ready = true;
+  self.emit('update-complete');
+}
+
+Fetcher.prototype.self_update = function() {
+  var self = this;
+  self.check_launcher();
+}
+
+Fetcher.prototype.check_launcher = function() {
+  var self = this;
+
+	var conf = {};
+	var file_name = "package.nw";
+	var options = url.parse(self.config.httpupdater.baseurl + file_name);
+	options.method = 'head';
+	var request = http.request(options, function(response) {
+		try {
+			conf = JSON.parse(fs.readFileSync(path.join(self.config.local_base_dir, './cache/cache.json')));
+		} catch (e) {
+			;
+		}
+		if (conf.etag == response.headers['etag']) {
+      self.emit('selfupdate-status', false);
+		} else {
+      self.emit('selfupdate-status', true);
+		}
+	});
+	request.end();
+}
+
+Fetcher.prototype.download_launcher() {
+  var self = this;
+	var file_name = "package.nw";
+	window.makedirs(path.dirname(path.join(self.config.local_base_dir, file_name)));
+	var request = http.get(self.config.httpupdater.baseurl + file_name, function(response) {
+		if (response.statusCode == 200) {
+			var downloaded = 0;
+			var speed = 0;
+			var start_time = new Date().getTime();
+			var file = fs.createWriteStream(path.join(self.config.local_base_dir, file_name));
+
+			window.localStorage.lastEtag = response.headers['etag'];
+			try {
+				fs.writeFileSync(path.join(self.config.local_base_dir, './cache/cache.json'), JSON.stringify({"etag": response.headers['etag']}, null, 4));
+			} catch (e) {
+				;
+			}
+
+			response.on('data', function (chunk) {
+				downloaded += chunk.length;
+				total = response.headers['content-length'];
+				duration = (new Date().getTime() - start_time) / 1000;
+				speed = downloaded / duration;
+				self.emit('selfupdate-progress', downloaded, total, speed);
+			});
+
+			response.on('end', function() {
+				file.end(function() {
+					self.emit('selfupdate-complete');
+				});
+			});
+
+			response.pipe(file);
+		}
+	});
+}
+
+Fetcher.prototype.download_manifest = function(cb) {
+  var self = this;
+  var buffer = "";
+  var request = http.get(this.config.httpupdater.baseurl + "files.txt", function(response) {
+		response.on('data', function(chunk) {
+			buffer += chunk;
+		});
+
+		response.on('end', function () {
+      self.files = (JSON.parse(buffer)).files;
+			if (cb !== undefined) {
+				var changed = true;
+				var hash = crypton.createHash('sha256');
+				hash.update(buffer);
+				var currentChecksum = hash.digest('hex');
+				if (window.localStorage.lastChksum == currentChecksum) {
+					changed = false;
+				} else {
+					window.localStorage.updateRequired = true;
+					window.localStorage.lastChksum = currentChecksum;
+				}
+				cb(changed);
+			}
+		});
+  });
+}
+
 Fetcher.prototype.update_payload = function() {
+  var self = this;
 	console.log('Fetcher.update_payload');
+
+  self.emit('update-started');
+  self.download_manifest(function(changed) {
+    if (changed || window.localStorage.updateRequired == "true") {
+      self.fetch_next_file();
+    } else {
+      self._payload_update_completed();
+    }
+  });
+}
+
+Fetcher.prototype.run_payload = function() {
+  var self = this;
+
+  spawn(path.join(self.config.local_base_dir, self.config.payload.executable), {detached: true});
 }
 
 Fetcher.prototype.fetch_file = function() {
 	this.emit('fetch-started');
 }
 
-Fetcher.prototype.verify_file = function() {
-	this.emit('verify-started');
+Fetcher.prototype.fetch_next_file = function() {
+  var self = this;
+
+  self.fetch_file();
+  if( self.current_file < self.files.length) {
+		var file_info = self.files[self.current_file++];
+    self.verify_file(file_info[0], file_info[1]);
+	} else {
+		self._payload_update_completed();
+	}
 }
 
-Fetcher.prototype.files = [];
-Fetcher.prototype.current_file = 0;
+Fetcher.prototype.verify_file = function(file_name, checksum) {
+  var self = this;
+  console.log('verify_file:', file_name, checksum);
+	self.emit('verify-started', self.current_file + 1, self.files.length);
+
+  var stream = fs.createReadStream(path.join(self.config.local_base_dir, file_name));
+  var verified = 0;
+  var size = 1;
+  var hash = crypton.createHash('sha256');
+
+  stream.on('open', function(fd) {
+    var stats = fs.fstatSync(fd);
+    size = stats.size;
+  });
+
+  stream.on('data', function(data) {
+    hash.update(data);
+    verified += data.length;
+    self.emit('verify-progress', verified, size);
+  });
+
+  stream.on('end', function() {
+    if(hash.digest('hex') == checksum) {
+      self.emit('verify-success');
+      console.log('Checksum ok for file ' + file_name);
+    } else {
+      self.emit('verify-failed', file_name);
+      console.log('Checksum WRONG for file ' + file_name);
+    }
+  });
+
+  stream.on('error', function(err) {
+    console.log('File not found');
+    self.emit('verify-failed', file_name);
+  });
+}
+
+Fetcher.prototype.download_file = function(file_name) {
+  var self = this;
+
+  console.log('download_file', file_name);
+  window.makedirs(path.dirname(path.join(self.config.local_base_dir, file_name)));
+  var request = http.get(self.config.httpupdater.baseurl + file_name, function(response) {
+    if (response.statusCode == 200)	{
+      self.emit('download-started', file_name);
+      var downloaded = 0;
+      var speed = 0;
+      var start_time = new Date().getTime();
+      var file = fs.createWriteStream(path.join(self.config.local_base_dir, file_name));
+
+      response.on('data', function (chunk) {
+        downloaded += chunk.length;
+        total = response.headers['content-length'];
+        duration = (new Date().getTime() - start_time) / 1000;
+        speed = downloaded / duration;
+        self.emit('download-progress', downloaded, total, speed);
+      });
+
+      response.on('end', function() {
+        file.end(function() {
+          self.emit('download-complete');
+        });
+      });
+
+      response.pipe(file);
+    }
+  });
+}
 
 module.exports = Fetcher;
--- src/main.js
+++ src/main.js
@@ -1,21 +1,18 @@
-var crypton = require('crypto');
 var gui = require('nw.gui');
-var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var spawn = require('child_process').spawn;
-var url = require('url');
 
 var Fetcher = require('./fetcher.js');
-var fetcher = new Fetcher();
-
-var state = 0;
-var local_base_dir = '.';
-var launcher_updated_required = true;
 
 var config = require('./package.json').fetcher;
+config.local_base_dir = '.';
+config.local_base_dir = path.dirname(process.execPath);
+var fetcher = new Fetcher(config);
 
-var local_base_dir = path.dirname(process.execPath);
+function exit() {
+  gui.App.quit();
+}
 
 function set_status(text) {
 	document.querySelector('#statusbar').textContent = text;
@@ -29,76 +26,16 @@ function set_progress_text(text) {
 	document.querySelector('x-progressbar').text(text);
 }
 
-function download_manifest(cb) {
-	set_status('Checking for updates');
-  var buffer = "";
-  var request = http.get(config.httpupdater.baseurl + "files.txt", function(response) {
-	response.on('data', function(chunk) {
-		buffer += chunk;
-	});
-
-	response.on('end', function () {
-		if (cb !== undefined) {
-			var changed = true;
-			var hash = crypton.createHash('sha256');
-			hash.update(buffer);
-			var currentChecksum = hash.digest('hex');
-			if (localStorage.lastChksum == currentChecksum) {
-				changed = false;
-			} else {
-				localStorage.updateRequired = true;
-				localStorage.lastChksum = currentChecksum;
-			}
-			cb(JSON.parse(buffer), changed);
-		}
-	});
-  });
-}
-
-function verify_file(file_name, checksum) {
-  var stream = fs.createReadStream(path.join(local_base_dir, file_name));
-  var verified = 0;
-  var size = 1;
-  var hash = crypton.createHash('sha256');
-
-  stream.on('open', function(fd) {
-	var stats = fs.fstatSync(fd);
-	size = stats.size;
-  });
-
-  stream.on('data', function(data) {
-    hash.update(data);
-    verified += data.length;
-		fetcher.emit('verify-progress', verified, size);
-  });
-
-  stream.on('end', function() {
-    if(hash.digest('hex') == checksum) {
-		fetcher.emit('verify-success');
-      console.log('Checksum ok for file ' + file_name);
-    } else {
-		fetcher.emit('verify-failed', file_name);
-      console.log('Checksum WRONG for file ' + file_name);
-    }
-  });
-
-  stream.on('error', function(err) {
-    console.log('File not found');
-		fetcher.emit('verify-failed', file_name);
-  });
-}
-
-fetcher.on('verify-started', function() {
-	set_progress_text('Verifying file ' + (fetcher.current_file + 1) + ' of ' + fetcher.files.length);
+fetcher.on('verify-started', function(a, b) {
+	set_status('Updating...');
+	set_progress_text('Verifying file ' + a + ' of ' + b);
 });
 
-fetcher.on('verify-progress', function(verified, size) {
-	set_progress(verified, size);
-});
+fetcher.on('verify-progress', set_progress);
 
-fetcher.on('verify-failed', download_file);
+fetcher.on('verify-failed', fetcher.download_file);
 
-fetcher.on('verify-success', fetch_next_file);
+fetcher.on('verify-success', fetcher.fetch_next_file);
 
 fetcher.on('download-started', function(file_name) {
 	console.log("downloading: " + file_name);
@@ -109,143 +46,50 @@ fetcher.on('download-progress', function(downloaded, total, speed) {
 	set_progress_text('Downloading file ' + (fetcher.current_file + 1) + ' of ' + fetcher.files.length + ', ' + (downloaded / (1024 * 1024)).toFixed(1) + ' MB of ' + (total / (1024 * 1024)).toFixed(1) + ' MB @ ' + (speed / 1024) .toFixed(0) + ' kB/sec');
 });
 
-fetcher.on('download-complete', fetch_next_file);
+fetcher.on('download-complete', fetcher.fetch_next_file);
 
-function fetch_next_file() {
-	fetcher.fetch_file();
-	if( fetcher.current_file < fetcher.files.length) {
-		var file_info = fetcher.files[fetcher.current_file++];
-		fetcher.verify_file();
-    		verify_file(file_info[0], file_info[1]);
+fetcher.on('selfupdate-status', function(updateAvailable) {
+	if(updateAvailable) {
+		fetcher.download_launcher();
 	} else {
-		fetcher.emit('update-complete');
+		fetcher.update_payload();
 	}
-}
+});
 
-function download_file(file_name) {
-	makedirs(path.dirname(path.join(local_base_dir, file_name)));
-	var request = http.get(config.httpupdater.baseurl + file_name, function(response) {
-		if (response.statusCode == 200)	{
-			fetcher.emit('download-started', file_name);
-			var downloaded = 0;
-			var speed = 0;
-			var start_time = new Date().getTime();
-			var file = fs.createWriteStream(path.join(local_base_dir, file_name));
+fetcher.on('selfupdate-progress', function(downloaded, total, speed) {
+	set_progress(downloaded, total);
+	set_progress_text('Downloading new launcher ' + (downloaded / (1024 * 1024)).toFixed(1) + ' MB of ' + (total / (1024 * 1024)).toFixed(1) + ' MB @ ' + (speed / 1024) .toFixed(0) + ' kB/sec');
+});
 
-			response.on('data', function (chunk) {
-				downloaded += chunk.length;
-				total = response.headers['content-length'];
-				duration = (new Date().getTime() - start_time) / 1000;
-				speed = downloaded / duration;
-				fetcher.emit('download-progress', downloaded, total, speed);
-			});
+fetcher.on('selfupdate-complete', function() {
+	spawn(process.execPath, {detached: true}).unref();
+	gui.App.quit();
+});
 
-			response.on('end', function() {
-				file.end(function() {
-					fetcher.emit('download-complete');
-				});
-			});
-
-			response.pipe(file);			
-		}
-	});
-}
-
-function check_launcher(cb) {
-	var conf = {};
-	var file_name = "package.nw";
-	var options = url.parse(config.httpupdater.baseurl + file_name);
-	options.method = 'head';
-	var request = http.request(options, function(response) {
-		try {
-			conf = JSON.parse(fs.readFileSync(path.join(local_base_dir, './cache/cache.json')));
-		} catch (e) {
-			;
-		}
-		if (conf.etag == response.headers['etag']) {
-			cb(false);
-		} else {
-			cb(true);
-		}
-	});
-	request.end();
-}
-
-function download_launcher(cb) {
-	var file_name = "package.nw";
-	makedirs(path.dirname(path.join(local_base_dir, file_name)));
-	var request = http.get(config.httpupdater.baseurl + file_name, function(response) {
-		if (response.statusCode == 200) {
-			var downloaded = 0;
-			var speed = 0;
-			var start_time = new Date().getTime();
-			var file = fs.createWriteStream(path.join(local_base_dir, file_name));
-			
-			localStorage.lastEtag = response.headers['etag'];
-			try {
-				fs.writeFileSync(path.join(local_base_dir, './cache/cache.json'), JSON.stringify({"etag": response.headers['etag']}, null, 4));
-			} catch (e) {
-				;
-			}
-
-			response.on('data', function (chunk) {
-				downloaded += chunk.length;
-				total = response.headers['content-length'];			
-				duration = (new Date().getTime() - start_time) / 1000;
-				speed = downloaded / duration;
-				set_progress(downloaded, total);
-				set_progress_text('Downloading new launcher ' + (downloaded / (1024 * 1024)).toFixed(1) + ' MB of ' + (total / (1024 * 1024)).toFixed(1) + ' MB @ ' + (speed / 1024) .toFixed(0) + ' kB/sec');
-			});
-
-			response.on('end', function() {
-				file.end(function() {
-					if (cb !== undefined) {
-						cb();
-					}	
-				});
-			});
-
-			response.pipe(file);	
-		}
-	});
-}
-
-function exit() {
-  gui.App.quit();
-}
+fetcher.on('update-started', function() {
+	set_status('Checking for updates');
+});
 
 fetcher.on('update-complete', function() {
 	console.log('fetch files completed');
 	localStorage.updateRequired = false;
 	set_status('Update complete - ready to launch.');
-	document.getElementById('btn').innerHTML = 'Run';
-	state = 3;
-	if (true) { //document.getElementById('autostart').checked) {
+	if (false) { //document.getElementById('autostart').checked) {
 		run();
-	} 
+	}
 });
 
 function run() {
-	if (launcher_updated_required) {
-		return;
-	}
-	if (state == 1) {
-		fetcher.current_file = 0;
-		state = 2;
-		set_status('Update in progress')
-		fetcher.update_payload();
-		fetch_next_file();
-	}
-	if (state == 3) {
+	if (fetcher.payload_ready) {
 		set_status('Launching application...');
-		spawn(path.join(local_base_dir, config.payload.executable), {detached: true});
+		fetcher.run_payload();
 		exit();
 	}
 }
 
 document.addEventListener('DOMContentLoaded', function(event) {
 	var win = gui.Window.get();
-	
+
 	if (localStorage.autostart === undefined) {
 		localStorage.autostart = false;
 	}
@@ -261,48 +105,14 @@ document.addEventListener('DOMContentLoaded', function(event) {
 	document.querySelector('#btn').addEventListener('click', run);
 	document.querySelector('#exit').addEventListener('click', exit);
 	document.querySelector('#settings').addEventListener('click', function() {
-		state = 1;
 		return false;
 	});
 
-	check_launcher(function(updateAvailable) {
+	if (gui.App.argv.length == 0) {
 		set_status('Checking launcher version');
-		if (updateAvailable) {
-			if (gui.App.argv.length == 0) {
-				download_launcher(function() {
-					spawn(process.execPath, {detached: true}).unref();
-					gui.App.quit();
-				});
-			} else {
-				launcher_updated_required = false;
-				console.log('Update available, but disabled via argument');
-				download_manifest(function(data, changed) {
-					fetcher.files = data.files;
-					if (changed || localStorage.updateRequired == "true") {
-						state = 1;
-					} else {
-						state = 3;
-						if (true) { //document.getElementById('autostart').checked) {
-							//run();
-						} 
-					}
-					run();
-				});
-			}
-		} else {
-			launcher_updated_required = false;
-			download_manifest(function(data, changed) {
-				fetcher.files = data.files;
-				if (changed || localStorage.updateRequired == "true") {
-					state = 1;
-				} else {
-					state = 3;
-					if (true) { //document.getElementById('autostart').checked) {
-						//run();
-					} 
-				}
-				run();
-			});
-		}
-	});
+		fetcher.self_update();
+	} else {
+		set_status('Update in progress')
+		fetcher.update_payload();
+	}
 });
